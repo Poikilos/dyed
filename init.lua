4508464e97503f15f1ad7a07f6ab5417d39de155
poikilos <7557867+poikilos@users.noreply.github.com>
2020-02-13 22:05:30 -0500

    initial commit

--- init.lua
+++ init.lua
@@ -1,6 +1,6 @@
 
 
-local colors = {
+local colorapi_dye_lookup = {
 -- The numbers here MUST match the colorapi_palette image's order.
     ['dye:white']      = 0,
     ['dye:grey']       = 1,  -- spelled "grey" upstream in dyes
@@ -17,9 +17,15 @@ local colors = {
     ['dye:red']        = 12,
     ['dye:magenta']    = 13,
     ['dye:pink']       = 14,
-    ['light_blue:dye'] = 15 -- reserved for future use
-}
 
+}
+local colorapi_colors = {'white', 'grey', 'dark_grey', 'black', 'violet', 'blue', 'cyan', 'dark_green', 'green', 'yellow', 'brown', 'orange', 'red', 'magenta', 'pink'}
+local colorapi_dyes = {'dye:white', 'dye:grey', 'dye:dark_grey', 'dye:black', 'dye:violet', 'dye:blue', 'dye:cyan', 'dye:dark_green', 'dye:green', 'dye:yellow', 'dye:brown', 'dye:orange', 'dye:red', 'dye:magenta', 'dye:pink'}
+if (rawget(_G, "lightblue")) then  -- TODO: make a light_blue mod
+    colorapi_colors[16] = "lightblue"
+    -- Minetest palette indices start at 0, so 15 is last (same as 16th Lua index):
+    colorapi_dye_lookup['light_blue:dye'] = 15
+end
 function colorapi.register_colorapi_node(name, def)
 -- special thanks: Linuxdirk on minetest forum f=47&t=19492
     def.paramtype2 = 'color'
@@ -34,7 +40,7 @@ function colorapi.register_colorapi_node(name, def)
         local meta = puncher:get_wielded_item():get_meta()
         -- TODO: Use the item metadata
         if minetest.registered_items[name].groups.dye ~= nil then
-            local color = colors[name] or false
+            local color = colorapi_dye_lookup[name] or false
             if color ~= false then
                 local node = minetest.get_node(pos)
                 node.param2 = color
@@ -43,4 +49,18 @@ function colorapi.register_colorapi_node(name, def)
         end
     end
     minetest.register_node(name, def)
+    for index, this_dye_name in ipairs(colorapi_dyes) do
+        minetest.register_craft({
+            type = "shapeless",
+            output = "colorapi:wool",
+            recipe = {"colorapi:wool", this_dye_name},
+        })
+        minetest.register_on_craft(
+            function(itemstack, player, old_craft_grid, craft_inv) {
+                -- TODO: check ingredients and set param2
+            }
+        )
+    end
 end
+
+
